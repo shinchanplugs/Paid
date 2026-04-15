4d789e1e21269ddabf61c2564489c2c758d6d772
Vishal Kumawat <shinchanplugs@gmail.com>
2026-04-15 21:54:23 +0530

    Update alexou.py

--- alexou.py
+++ alexou.py
@@ -1,154 +1,29 @@
-__ENC_OWNER__ = "SHINCHAN"
-__TELEGRAM__ = "@vishaldied"
-__TELEGRAM_CHANNEL__ = "@shinchanplugs"
-__GROUP_CHAT__ = "@shinchankagc"
 
-def _sH1nChAn_治損要治大校泊ベ策ピ反木(key, data):
-    key_ord = ord(key) if isinstance(key, str) else key
-    result = []
-    for val in data:
-        result.append(chr(val ^ key_ord))
-    return ''.join(result)
-
-def _sH1nChAn_そ左ブ地害拡革ヵべ聞球治(data, keys):
-    import base64
-    encoded = base64.b85decode(data.encode())
-    encoded = bytes([b ^ int(keys[2]) for b in encoded])
-    encoded = base64.b64decode(encoded)
-    encoded = bytes([b ^ int(keys[1]) for b in encoded])
-    encoded = encoded[::-1]
-    encoded = bytes([b ^ int(keys[0]) for b in encoded])
-    return encoded
-
-def _sH1nChAn_ヵブがが分治ゆ和ズレ業業():
-    _shinchan_原賰保売過稫齢生龚女 = 1006
-    _shinchan_聞る損が音剛種組剛校 = str(_shinchan_原賰保売過稫齢生龚女)
-    return _shinchan_聞る損が音剛種組剛校 ^ 188
-
-def _sH1nChAn_大達海森保書動省北意齢団():
-    _shinchan_暶和枯文害業哴そ要意 = 9693
-    _shinchan_嗼龙ぢ治鵈暶ユ損森金 = max(_shinchan_暶和枯文害業哴そ要意)
-    return _shinchan_嗼龙ぢ治鵈暶ユ損森金 ^ 82
-
-def _sH1nChAn_済語正齢経ホヌ名申球分言():
-    _shinchan_そ組龘業憻攤刼竾発保 = 4676
-    _shinchan_語よ蜔政名龗仲泊運鱗 = abs(_shinchan_そ組龘業憻攤刼竾発保)
-    return _shinchan_語よ蜔政名龗仲泊運鱗 ^ 169
-
-def _sH1nChAn_ヌピ原道過ヌ物右ベ宿ユ団():
-    _shinchan_坸要明売国壍龛果賰済 = 2231
-    _shinchan_難同全音ヘ達龖明ぢヵ = abs(_shinchan_坸要明売国壍龛果賰済)
-    return _shinchan_難同全音ヘ達龖明ぢヵ ^ 240
-
-def _sH1nChAn_害閉員子左仲因聞明種べ道():
-    _shinchan_騺る業政易ィ金龚果森 = 6812
-    _shinchan_語和龘書功業ヘ赻策虤 = str(_shinchan_騺る業政易ィ金龚果森)
-    return _shinchan_語和龘書功業ヘ赻策虤 ^ 118
-
-def _sH1nChAn_金係和拡ブウ難経レ文策芸():
-    import hashlib
-    expected = "c7fe89d393018d9b"
-    current = hashlib.md5((__ENC_OWNER__ + __TELEGRAM__ + __TELEGRAM_CHANNEL__ + __GROUP_CHAT__).encode()).hexdigest()[:16]
-    if current != expected:
-        raise SystemExit('Validation failed')
-    return True
-
-_shinchan_名捩全樕憻抻抻壍正蜔 = __import__(_sH1nChAn_治損要治大校泊ベ策ピ反木('虗', [34360, 34340]))
-_shinchan_龜仲同レ聁賰安鵈海煾 = __import__(_sH1nChAn_治損要治大校泊ベ策ピ反木('缋', [32617, 32618, 32632, 32622, 32573, 32575]))
-_shinchan_発語ホ龘左鵳係道組外 = __import__(_sH1nChAn_治損要治大校泊ベ策ピ反木('樕', [27247, 27257, 27260, 27255]))
-_shinchan_龚護鵈策わ因団ウ大仕 = __import__(_sH1nChAn_治損要治大校泊ベ策ピ反木('骸', [39637, 39641, 39626, 39627, 39632, 39641, 39636]))
-_shinchan_剛攤ぅ訴北鱗険悑易濠 = __import__(_sH1nChAn_治損要治大校泊ベ策ピ反木('竾', [31373, 31367, 31373]))
-_shinchan_ヌは龜ベ濠反姄洪町龘 = __import__(_sH1nChAn_治損要治大校泊ベ策ピ反木('啇', [21815, 21803, 21798, 21811, 21793, 21800, 21813, 21802]))
-_shinchan_そ拡長悑員る仕れ悑竾 = __import__(_sH1nChAn_治損要治大校泊ベ策ピ反木('鰞', [40054, 40063, 40045, 40054, 40050, 40055, 40060]))
+from colorama import Fore, Style, init
 
 
-try:
-    _sH1nChAn_ヵブがが分治ゆ和ズレ業業()
-except:
-    pass
-try:
-    _sH1nChAn_大達海森保書動省北意齢団()
-except:
-    pass
-try:
-    _sH1nChAn_済語正齢経ホヌ名申球分言()
-except:
-    pass
+init(autoreset=True)
 
-_shinchan_治ワ球騸憻槀缋摳ベ騺 = _sH1nChAn_金係和拡ブウ難経レ文策芸()
+WIDTH = 30
 
-_shinchan_聁坸龘理損龞学龚ホ保 = _shinchan_そ拡長悑員る仕れ悑竾.sha256((__ENC_OWNER__ + __TELEGRAM__ + __TELEGRAM_CHANNEL__ + __GROUP_CHAT__).encode()).hexdigest()
-if len(_shinchan_聁坸龘理損龞学龚ホ保) != 64:
-    raise SystemExit('Hash validation failed')
 
-try:
-    _sH1nChAn_ヌピ原道過ヌ物右ベ宿ユ団()
-except:
-    pass
-try:
-    _sH1nChAn_害閉員子左仲因聞明種べ道()
-except:
-    pass
+def frame_line() -> str:
+	return "═" * WIDTH
 
-_shinchan_文壍潰因龝左壍喰果全 = 8947 ^ 12
-_shinchan_仕喰作要騸が憻ち海種 = len(str(68793)) * 2
-_shinchan_騺捩業龜龛捩鵈国運省 = abs(667) + 57
-_shinchan_洪業正棽過文啇北訴和 = 9419 ^ 178
-_shinchan_团槀棽語タ鵈大棽赻賰 = len(str(95531)) * 7
-_shinchan_ヌ缋龙摳球はピ羽赻経 = abs(576) + 78
-_shinchan_更名ユ鵈係語嗼ウ鰞憻 = 5140 ^ 129
-_shinchan_安ピ動器木国煾キ蜔要 = len(str(20138)) * 10
-_shinchan_北嗼ィ汵谑作音虤龝因 = abs(-531) + 91
-_shinchan_鸞团煾子宿校外は閉よ = 2031 ^ 216
-_shinchan_濠運剛訴生坸国棽龛示 = len(str(88260)) * 9
-_shinchan_名ホ同運町地易憻哴羽 = abs(558) + 50
-_shinchan_樕害女音易語ぢ北地組 = 9238 ^ 91
-_shinchan_名龘そ渾牃龞閉険生姄 = len(str(33251)) * 3
-_shinchan_龬森稫稫姄因大地運儈 = abs(581) + 27
 
-鵈龬氻羽憻牃煾鵳绿牃抻攤缋睥醁抻睥暶龜煾赻谑鵅哴秢 = _shinchan_ヌは龜ベ濠反姄洪町龘.system().lower()
+def center(text: str) -> str:
+	return text.center(WIDTH)
 
-啇羽啇鵳蜔龛汵龜騌姄攤騌鵳攤氻蒧睥稫 = 99
-龬濠龙龘枯攤绿汵音龘龖龜竾龝煾秢龞憻 = 3
-棽音渾龜鵈碔醁缋滰龚攤龞潰谑蜔鰞醁暶 = 181
 
-exec(_sH1nChAn_治損要治大校泊ベ策ピ反木('捩', [25398, 25375, 25352, 25349, 25344, 25357, 25352, 25373, 25344, 25350, 25351, 25398, 25346, 25356, 25360, 25417, 25428, 25417, 25422, 25354, 25438, 25359, 25356, 25425, 25424, 25357, 25434, 25424, 25434, 25433, 25432, 25425, 25357, 25424, 25355, 25422]))
-if '_validation_key' not in locals() or _validation_key != "c7fe89d393018d9b":
-    raise SystemExit('Tamper detected')
+title = " ACCESS NOTICE "
+message = "This file has expired.\nPlease contact :@vishaldied for Paid Access."
+contact = "Contact: @vishaldied"
 
-喰骸嗼龘龛鸞牃攤音龬赻赻嗼睥捩槀騺坸槀羽 = "*xb|e^XJ$4{>_cX+K1-T+syd>=Y#q4fZ_W6-<sss(tw1)*U{$M#@+w(!u|Bd(ct^S`{IrFgY^I3+|&Nn)7zfZ;?soB-iO%n*_!&>*_!z8+UD)z-}c*$)85j;<&B5_^6=W``2Y06f`Ihb)8352{_KVChV6jFfb_zL-}&Ewp4!ZSgys0{p3(dI&E)Xeg~sB-`rq)!_x9lU|KQsA?c~IsjN*;m(eQ=-z{1r3=hD#F(DwF)_JfAPfY;jW"
-龞蜔姄鱗騸騺蜔聁騸嗼鵳羽抻赻姄 = 3
-睥蒧龜碔刼捩騌龝鵳缋龙赻缋虤聁鰞龛擆龙龞 = "{oaJag528en)v>ngXHD$^z((|;=}Uu&+^gu|BU2@?2Pxv-Gs%^hxmcp-`VHc$I;%}+M3qF*3sC{!}#{V!N<pd_QAlx!sp)Q!Smkb&)LJx`u)-8+}nr8&F}Ec+}GQH&FAIe#M|ci`{V4z$M4+U!T#of&4urb^V-JUjl<-`;(-3xg8R~p&w!fOhuw|s^UsaU-_6X>{MOmg+~3^X@WuRv_t?UP_T7#B(9w+7jMDn?!Q1zS+}-}<?~V5I"
-坸儈龘抻暶碔谑鱗憻羽谑騸龝賰羽 = 2
-鱗龘鵈枯鰞绿棽音抻龞擆煾虤憻坸碔竾赻煾龘 = "@rK;r`_0>i--hwWgpJYq)Pl{PfYbQc!|%-T!_2|d`qKFEhx^Cf#^s*j`_kds)Q$Y!(1hg2+W!8{?fAj`|Lpg}jo<Iy{EgIy`i<w>;P{1y;oRTs(9g`?{QSVo{L}b{*!|DUgu?cRgXWt4%!l;H?8oHX%>CK>@!j0|`279F)Qs}``rGe-%=-F;o`J*s<I;fUf%oQ(+}QTU#Esbf<KyL?^wIc-)ZUtb-Q(c&{F?FOfc)@`h4J~+?1bjq"
-剛龛槀煾龝棽濠鵈攤刼哴蒧枯濠漍 = 1
-悑鰞騺碔鵳哴漍騺潰龜氻憻龘漍牃汵濠樕煾賰 = "fc)k2-q_UE&FtaW)cNJd-_OARhVYD@h3wMe`uzQ#!tdUV@x|MkhT)!vg5J{c@zL$Y?bL&T#-7@Y)WZFOh5pUc_~Fcj*~j>T`po@};q(57{F>qS=HZR|)YgLb`Q!cm^7zs6#qq|>+J&0U%+K0|!v2NA%-iLj#G3Q=z`^^6!Nc+Y!Q=Yejnw&t&-Z}#%-sEf@ZH<l?a=Jd#O2Y#+3@p?f&b0%#`?q5hsEBF!{N-HhS#3h!NQus;`;sf"
-漍竾儈抻鵅鵳龞团鵳氻竾哴騺虗绿 = 0
-
-exec(_sH1nChAn_治損要治大校泊ベ策ピ反木('儈', [20833, 20846, 20776, 20838, 20839, 20860, 20776, 20841, 20836, 20836, 20768, 20819, 20823, 20823, 20813, 20806, 20811, 20823, 20807, 20831, 20806, 20813, 20826, 20823, 20823, 20776, 20789, 20789, 20776, 20778, 20827, 20800, 20801, 20806, 20811, 20800, 20809, 20806, 20778, 20772, 20776, 20823, 20823, 20828, 20813, 20804, 20813, 20815, 20826, 20809, 20805, 20823, 20823, 20776, 20789, 20789, 20776, 20778, 20808, 20862, 20833, 20859, 20832, 20841, 20836, 20844, 20833, 20845, 20844, 20778, 20772, 20776, 20823, 20823, 20828, 20813, 20804, 20813, 20815, 20826, 20809, 20805, 20823, 20811, 20800, 20809, 20806, 20806, 20813, 20804, 20823, 20823, 20776, 20789, 20789, 20776, 20778, 20808, 20859, 20832, 20833, 20838, 20843, 20832, 20841, 20838, 20856, 20836, 20861, 20847, 20859, 20778, 20772, 20776, 20823, 20823, 20815, 20826, 20807, 20829, 20824, 20823, 20811, 20800, 20809, 20828, 20823, 20823, 20776, 20789, 20789, 20776, 20778, 20808, 20859, 20832, 20833, 20838, 20843, 20832, 20841, 20838, 20835, 20841, 20847, 20843, 20778, 20821, 20769, 20786, 20776, 20858, 20841, 20833, 20859, 20845, 20776, 20827, 20849, 20859, 20860, 20845, 20837, 20813, 20848, 20833, 20860, 20768, 20783, 20800, 20845, 20841, 20844, 20845, 20858, 20776, 20862, 20841, 20836, 20833, 20844, 20841, 20860, 20833, 20839, 20838, 20776, 20846, 20841, 20833, 20836, 20845, 20844, 20783, 20769]))
-
-try:
-    _shinchan_牃樕ヌ苛棽鸞苛骸稫作 = 悑鰞騺碔鵳哴漍騺潰龜氻憻龘漍牃汵濠樕煾賰 + 鱗龘鵈枯鰞绿棽音抻龞擆煾虤憻坸碔竾赻煾龘 + 睥蒧龜碔刼捩騌龝鵳缋龙赻缋虤聁鰞龛擆龙龞 + 喰骸嗼龘龛鸞牃攤音龬赻赻嗼睥捩槀騺坸槀羽
-    _shinchan_鱗ぅ森攤更潰策生擆宿 = [啇羽啇鵳蜔龛汵龜騌姄攤騌鵳攤氻蒧睥稫, 龬濠龙龘枯攤绿汵音龘龖龜竾龝煾秢龞憻, 棽音渾龜鵈碔醁缋滰龚攤龞潰谑蜔鰞醁暶]
-    槀聁鵈羽摳悑騺醁剛缋醁汵蒧漍苛攤秢竾鵳騸醁擆樕刼悑 = _sH1nChAn_そ左ブ地害拡革ヵべ聞球治(_shinchan_牃樕ヌ苛棽鸞苛骸稫作, _shinchan_鱗ぅ森攤更潰策生擆宿)
-    樕苛蒧蜔龖竾喰龬赻煾枯龗剛虤啇龬賰騸龝汵 = _shinchan_龚護鵈策わ因団ウ大仕.loads(_shinchan_発語ホ龘左鵳係道組外.decompress(槀聁鵈羽摳悑騺醁剛缋醁汵蒧漍苛攤秢竾鵳騸醁擆樕刼悑))
-    exec(樕苛蒧蜔龖竾喰龬赻煾枯龗剛虤啇龬賰騸龝汵)
-except Exception as e:
-    raise SystemExit('Execution failed')
-
-try:
-    exec(軮龜攤憻龘憻鵅龚汵軮龚龜剛蜔睥龝樕龬抻槀, 哴鵳坸虤騸醁渾悑赻虤姄濠氻龚秢樕缋騺鵳抻苛龙睥碔坸儈赻汵秢虗谑憻虤鱗擆)
-except:
-    pass
-try:
-    exec(蜔暶蒧渾虤牃缋鰞抻哴绿嗼龜龗喰鵳蒧鵳龬抻刼龛睥鵳, 氻哴煾鵈擆龛氻喰枯龘剛騺龚龙龛剛悑壍龜绿龝鰞蒧槀鱗捩睥姄擆鵈)
-except:
-    pass
-try:
-    exec(摳賰鸞賰賰龙虗睥刼騌哴龝刼濠氻秢团攤赻苛擆暶蒧羽秢騌龚槀煾汵羽稫騌, 龬摳蒧虗憻氻赻虗騺氻樕喰鵈龞坸摳醁蜔鱗哴蜔龗悑剛啇)
-except:
-    pass
-try:
-    exec(儈騸骸鰞壍龘壍摳漍汵鸞氻賰氻虤鵅睥谑碔壍坸缋姄滰, 軮睥秢嗼竾啇潰鵈龛攤龛渾龞稫摳槀滰谑骸鸞虤憻虗牃竾稫憻壍龝睥蒧樕潰秢)
-except:
-    pass
+print(Fore.RED + Style.BRIGHT + frame_line())
+print(Fore.RED + Style.BRIGHT + center(title))
+print(Fore.RED + Style.BRIGHT + frame_line())
+print()
+print(Fore.YELLOW + Style.BRIGHT + center("⚠  " + message))
+print(Fore.CYAN + center(contact))
+print()
+print(Fore.RED + frame_line())
